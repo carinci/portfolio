ea82562476cd1374c110ab37e2d8a83bc20484dc
Chris Carinci <cc608@le.ac.uk>
2024-07-29 19:47:42 +0100

    Add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Header from './Header'
 import About from './sections/About'
 import Projects from './sections/Projects'
 import Contact from './sections/Contact'
+import Hero from './sections/Hero'
 
 export default function App() {
   return (
     <div className="font-mono">
       <Header />
+      <Hero />
       <About />
       <Projects />
       <Contact />
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,17 @@
 export default function Hero() {
-  return <></>
+  return (
+    <section className="bg-black px-4">
+      <div className="container mx-auto grid gap-6 pb-32 pt-16 text-white">
+        <p className="text-xl sm:text-2xl">Hi, I'm</p>
+        <p className="text-6xl font-bold sm:text-8xl">
+          <span className="bg-violet-600 leading-snug text-black">
+            Chris
+            <br />
+            Carinci.
+          </span>
+        </p>
+        <p className="text-xl sm:text-2xl">Full Stack Developer.</p>
+      </div>
+    </section>
+  )
 }
